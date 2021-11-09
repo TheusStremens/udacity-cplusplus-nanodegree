c43d06c0f1ac639c8a281b98b0edd7a09426b41b
TheusStremens <matheus.beep@gmail.com>
2021-11-09 11:10:31 -0300

    Make the main accept the number of processes as an argument

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,7 +1,15 @@
 #include "ncurses_display.h"
 #include "system.h"
+#include <iostream>
 
-int main() {
+int main(int argc,  char** argv)
+{
   System system;
-  NCursesDisplay::Display(system);
+  if (argc > 1)
+  {
+    int n = std::stoi(argv[1]);
+    NCursesDisplay::Display(system, n);
+  }
+  else
+    NCursesDisplay::Display(system);
 }
